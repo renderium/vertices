2d32c68b4d9eae6bd55b669b114e52f5bbcb0acb
Vladimir Bykov <broadsw.rd@yandex.ru>
2018-10-06 10:00:12 +0300

    Init impl of wasm vertices

--- wasm/vertices.c
+++ wasm/vertices.c
@@ -1,1 +1,16 @@
+#include "vertices.h"
 
+const size_t VERTEX_SIZE = sizeof(Vertex);
+
+char* verticesCreate (unsigned int length) {
+  return (char *)calloc(length, VERTEX_SIZE);
+}
+
+void verticesRealloc (char* vertices, unsigned int length) {
+  realloc(vertices, length * VERTEX_SIZE);
+}
+
+Vertex* verticesGet (char* vertices, unsigned int idx) {
+  Vertex* vertex = vertices + idx * VERTEX_SIZE;
+  return vertex;
+}
--- wasm/vertices.h
+++ wasm/vertices.h
@@ -1,1 +1,40 @@
+#include <stdlib.h>
 
+typedef struct {
+  short x;
+  short y;
+} Position;
+
+typedef struct {
+  unsigned short s;
+  unsigned short t;
+} Texture;
+
+typedef struct {
+  unsigned char r;
+  unsigned char g;
+  unsigned char b;
+  unsigned char a;
+} Color;
+
+typedef struct {
+  Position position;
+  Position center;
+  Texture texture;
+  Color color;
+  short theta;
+  unsigned char linearGradient;
+  unsigned char radialGradient;
+} Vertex;
+
+const size_t VERTEX_SIZE;
+
+char* verticesCreate (unsigned int length);
+
+void verticesRealloc (char* vertices, unsigned int length);
+
+Vertex* verticesGet (char* vertices, unsigned int idx);
+
+void verticesSetAlpha (Vertex* vertex, float alpha);
+
+void verticesSetTheta (Vertex* vertex, float theta);
