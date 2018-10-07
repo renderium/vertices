5d0523aebdf82de6a81d1a4570f2b3c41e9e3276
Vladimir Bykov <broadsw.rd@yandex.ru>
2018-10-07 09:28:16 +0300

    Wasm vertices

--- wasm/vertices.c
+++ wasm/vertices.c
@@ -1,6 +1,7 @@
 #include "vertices.h"
 
 const size_t VERTEX_SIZE = sizeof(Vertex);
+const float TWO_PI = 6.283185307179586f;
 
 char* verticesCreate (unsigned int length) {
   return (char *)calloc(length, VERTEX_SIZE);
@@ -13,4 +14,12 @@ void verticesRealloc (char* vertices, unsigned int length) {
 Vertex* verticesGet (char* vertices, unsigned int idx) {
   Vertex* vertex = vertices + idx * VERTEX_SIZE;
   return vertex;
+}
+
+void verticesSetAlpha (Vertex* vertex, float alpha) {
+  vertex->color.a = (unsigned char)(alpha * 0xff); 
+}
+
+void verticesSetTheta (Vertex* vertex, float theta) {
+  vertex->theta = (short)(fmod(theta, TWO_PI) / TWO_PI * 0x7fff);
 }
--- wasm/vertices.h
+++ wasm/vertices.h
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct {
   short x;
